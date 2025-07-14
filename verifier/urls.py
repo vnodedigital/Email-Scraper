4f0e3d168832e481fe456adde9a88deb732b2a03
vnode <digitalvnode@gmail.com>
2025-07-14 12:18:34 +0600

    verifier template error fix

--- verifier/urls.py
+++ verifier/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 app_name = 'verifier'
 urlpatterns = [
     # ... other urls ...
-    path('verify-email/<int:result_id>/', views.verify_email, name='verifier'),
+    # path('verify-email/<int:result_id>/', views.verify_email, name='verifier'),
     #path('save-valid-emails/<int:result_id>/', views.save_valid_emails, name='save_valid_emails'),
     path('verify-emails/', views.verify_emails, name='verify_emails'),
     # path for email_verification_confirm should also be added
--- verifier/views.py
+++ verifier/views.py
@@ -15,28 +15,5 @@ import dns.resolver
 
 
 @login_required
-def verify_email(request, result_id):
-    result = get_object_or_404(ScrapedFromGoogle, pk=result_id)
-    emails = result.emails or []
-
-    valid_emails = []
-    invalid_emails = []
-
-    for email in emails:
-        try:
-            validate_email(email)
-            valid_emails.append(email)
-        except EmailNotValidError:
-            invalid_emails.append(email)
-
-    return render(request, 'verifier/verify_email.html', {
-        'result': result,
-        'valid_emails': valid_emails,
-        'invalid_emails': invalid_emails
-    })
-
-
-
-@login_required
 def verify_emails(request):
     return render(request, 'verifier/verifier.html')
